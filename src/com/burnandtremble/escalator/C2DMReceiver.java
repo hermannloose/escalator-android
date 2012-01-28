ecec7dc70e96cc686decfbfb14f52d8411dab0f7
Hermann Loose <hermannloose@gmail.com>
2012-01-28 19:37:14 +0100

    Add empty C2DMReceiver.

---
+++ src/com/burnandtremble/escalator/C2DMReceiver.java
@@ -0,0 +1,15 @@
+package com.burnandtremble.escalator;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * @author Hermann Loose (hermannloose@gmail.com)
+ */
+public class C2DMReceiver extends BroadcastReceiver {
+  @Override
+  public void onReceive(Context arg0, Intent arg1) {
+    // TODO Auto-generated method stub
+  }
+}
