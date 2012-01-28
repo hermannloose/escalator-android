6328789f3a8d78191c924db7ed2c870dd2a46c09
Hermann Loose <hermannloose@gmail.com>
2012-01-28 20:09:48 +0100

    Handle registration in C2DMReceiver.
    This is largely copied from Google's C2DM site. Values are stored in shared
    preferences for now to play around and get a basic workflow set up.

--- src/com/burnandtremble/escalator/C2DMReceiver.java
+++ src/com/burnandtremble/escalator/C2DMReceiver.java
@@ -3,13 +3,45 @@ package com.burnandtremble.escalator;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 
 /**
  * @author Hermann Loose (hermannloose@gmail.com)
  */
 public class C2DMReceiver extends BroadcastReceiver {
   @Override
-  public void onReceive(Context arg0, Intent arg1) {
-    // TODO Auto-generated method stub
+  public void onReceive(Context context, Intent intent) {
+    if (intent.getAction().equals("com.google.android.c2dm.intent.REGISTRATION")) {
+      handleRegistration(context, intent);
+    } else if (intent.getAction().equals("com.google.android.c2dm.intent.RECEIVE")) {
+      handleMessage(context, intent);
+    }
+  }
+
+  private void handleMessage(Context context, Intent intent) {
+    // TODO(hermannloose): Implement C2DMReceiver#handleMessage.
+  }
+
+  private void handleRegistration(Context context, Intent intent) {
+    String registration = intent.getStringExtra("registration_id");
+    if (intent.getStringExtra("error") != null) {
+
+    } else if (intent.getStringExtra("unregistered") != null) {
+      SharedPreferences settings = context.getSharedPreferences("escalatorPush", 0);
+      SharedPreferences.Editor editor = settings.edit();
+
+      editor.putBoolean("registered", false);
+      editor.remove("registration_id");
+
+      editor.commit();
+    } else if (registration != null) {
+      SharedPreferences settings = context.getSharedPreferences("escalatorPush", 0);
+      SharedPreferences.Editor editor = settings.edit();
+
+      editor.putBoolean("registered", true);
+      editor.putString("registration_id", registration);
+
+      editor.commit();
+    }
   }
 }
